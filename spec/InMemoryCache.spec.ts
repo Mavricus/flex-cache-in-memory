031864abe3bbbe73905aea01c2f1cec57fe983a3
Mavricus <mamakc61@gmail.com>
2020-09-30 23:44:33 +0300

    Improvements
    - Adds unit-tests

---
+++ spec/InMemoryCache.spec.ts
@@ -0,0 +1,96 @@
+import { InMemoryCache } from '../src/InMemoryCache';
+
+jest.useFakeTimers();
+
+describe('InMemoryCache', () => {
+    let cache: InMemoryCache;
+    let storage: { [key: string]: { timere: NodeJS.Timer; data: unknown } };
+
+    beforeEach(() => {
+        storage = {};
+        cache = new InMemoryCache(storage);
+    });
+
+    describe('set', () => {
+        it('should assign to key the value',  () => {
+            return cache.set('a', 123, 5000)
+                        .then(() => expect(storage['a'].data).toBe(123));
+        });
+
+        it('should update the value if it presents',  () => {
+            return cache.set('a', 123, 5000)
+                        .then(() => expect(storage['a'].data).toBe(123))
+                        .then(() => cache.set('a', 321, 5000))
+                        .then(() => expect(storage['a'].data).toBe(321));
+        });
+    });
+
+    describe('delete', () => {
+        it('should delete data from the cache by key', () => {
+            return cache.set('a', 123, 5000)
+                        .then(() => expect(storage['a'].data).toBe(123))
+                        .then(() => cache.delete('a'))
+                        .then(() => expect(storage['a']).toBeUndefined());
+        });
+        it('should delete data from the cache by key', () => {
+            return Promise.resolve(expect(storage['a']).toBeUndefined())
+                        .then(() => expect(cache.delete('a')).resolves.toBeFalsy());
+        });
+    });
+
+    describe('get', () => {
+        it('should return data from the cache by key', () => {
+            return cache.set('a', 123, 5000)
+                        .then(() => expect(storage['a'].data).toBe(123))
+                        .then(() => expect(cache.get('a')).resolves.toBe(123));
+        });
+        it('should return null if there is no data by key', () => {
+            return Promise.resolve(expect(storage['a']).toBeUndefined())
+                          .then(() => expect(cache.get('a')).resolves.toBeNull());
+        });
+    });
+
+    describe('timeout', () => {
+        it('should delete data on timeout', () => {
+            return cache.set('a', 123, 5000)
+                        .then(() => expect(cache.get('a')).resolves.toBe(123))
+                        .then(() => jest.advanceTimersByTime(5000))
+                        .then(() => expect(cache.get('a')).resolves.toBeNull())
+        });
+        it('should delete data on timeout of very last set', () => {
+            return cache.set('a', 123, 5000)
+                        .then(() => expect(cache.get('a')).resolves.toBe(123))
+                        .then(() => jest.advanceTimersByTime(3000))
+                        .then(() => cache.set('a', 456, 5000))
+                        .then(() => expect(cache.get('a')).resolves.toBe(456))
+                        .then(() => jest.advanceTimersByTime(3000))
+                        .then(() => expect(cache.get('a')).resolves.toBe(456))
+                        .then(() => jest.advanceTimersByTime(2000))
+                        .then(() => expect(cache.get('a')).resolves.toBeNull());
+        });
+        it('should delete data on its personal timeout', () => {
+            return cache.set('a', 123, 5000)
+                        .then(() => cache.set('b', 456, 2500))
+                        .then(() => jest.advanceTimersByTime(2000))
+                        .then(() => expect(cache.get('a')).resolves.toBe(123))
+                        .then(() => expect(cache.get('b')).resolves.toBe(456))
+                        .then(() => jest.advanceTimersByTime(2000))
+                        .then(() => expect(cache.get('a')).resolves.toBe(123))
+                        .then(() => expect(cache.get('b')).resolves.toBeNull())
+                        .then(() => jest.advanceTimersByTime(2000))
+                        .then(() => expect(cache.get('a')).resolves.toBeNull())
+                        .then(() => expect(cache.get('b')).resolves.toBeNull());
+        });
+        it('should stop timer if data was deleted', () => {
+            return cache.set('a', 123, 5000)
+                        .then(() => jest.advanceTimersByTime(2000))
+                        .then(() => cache.delete('a'))
+                        .then(() => jest.advanceTimersByTime(2000))
+                        .then(() => cache.set('a', 456, 5000))
+                        .then(() => jest.advanceTimersByTime(2000))
+                        .then(() => expect(cache.get('a')).resolves.toBe(456))
+                        .then(() => jest.advanceTimersByTime(5000))
+                        .then(() => expect(cache.get('a')).resolves.toBeNull());
+        });
+    });
+});
--- src/InMemoryCache.ts
+++ src/InMemoryCache.ts
@@ -1,20 +1,38 @@
 import { ICacheController } from '@flex-cache/types';
 
+interface ICacheScope {
+    timer?: NodeJS.Timer;
+    data: unknown;
+}
+
 export class InMemoryCache implements ICacheController {
-    private storage: { [key: string]: unknown } = {};
+    constructor(private storage: { [key: string]: ICacheScope }) {
+    }
 
     delete<T>(name: string): Promise<void> {
+        const scope = this.storage[name];
+        if (scope != null && scope.timer != null) {
+            clearTimeout(scope.timer);
+        }
+
         delete this.storage[name];
         return Promise.resolve();
     }
 
     get<T>(name: string): Promise<T> {
-        return Promise.resolve(this.storage[name]);
+        const data = this.storage[name] != null ? this.storage[name].data : null;
+        return Promise.resolve(data as T);
     }
 
     set<T>(name: string, data: T, ttl: number): Promise<void> {
-        this.storage[name] = data;
-        return Promise.resolve();
-    }
+        let newScope: ICacheScope = { data };
+        if (ttl !== Infinity) {
+            newScope.timer = setTimeout(() => this.delete(name), ttl);
+        }
 
+        return this.delete(name)
+            .then(() => {
+                this.storage[name] = newScope;
+            });
+    }
 }
